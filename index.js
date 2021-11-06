e4d56b4858250f1aad4e9f4ecff838256c984990
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-05 23:44:18 -0400

    Add isAllowed() to check transaction is allowed or not

--- index.js
+++ index.js
@@ -20,19 +20,36 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+
   commit() {
+    if(!this.isAllowed()){
+      return false
+    }
     this.time = new Date();
     this.account.addTransaction(this);
+    return true;
    }
 }
 
 class Withdrawal extends Transaction {
+  isAllowed() {
+    if(this.account.balance - this.amount < 0){
+      return false;
+    }
+      return true;
+  }
   get value(){
      return -this.amount;
   }
 }
 
 class Deposite extends Transaction{
+  isAllowed(){
+    if(this.amount < 0) {
+      return false;
+    }
+    return true;
+  }
   get value(){
    return this.amount;
   }
@@ -46,13 +63,26 @@ class Deposite extends Transaction{
 const myAccount = new Account('snow-patrol');
 t3 = new Deposite(120, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
+// console.log('Transaction 3:', t3);
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
 
 t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2);
 
+t4 = new Withdrawal(59.76, myAccount);
+t4.commit();
+
+t7 = new Withdrawal(5.76, myAccount);
+t7.commit();
+console.log(t7.commit());
+t5 = new Deposite(2, myAccount);
+t5.commit();
+
+
+t6 = new Deposite(-2.2, myAccount);
+t6.commit();
+console.log(t6.commit());
 console.log('Balance:', myAccount.balance);
