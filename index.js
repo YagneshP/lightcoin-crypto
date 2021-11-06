411aff53f7309dda388e444fadfa19dc38018bbf
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-05 22:47:19 -0400

    Refactor commit method and utilized get from class

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+  commit() {
+    this.account.balance += this.value;
+   }
 }
 
 class Withdrawal extends Transaction {
-
-  commit() {
-   this.account.balance -= this.amount;
+  get value(){
+     return -this.amount;
   }
-
 }
 
 class Deposite extends Transaction{
-
-  commit() {
-    this.account.balance += this.amount;
+  get value(){
+   return this.amount;
   }
 }
 
@@ -35,6 +35,9 @@ class Deposite extends Transaction{
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account('snow-patrol');
+t3 = new Deposite(120, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
@@ -42,8 +45,5 @@ console.log('Transaction 1:', t1);
 t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
-
-t3 = new Deposite(120, myAccount);
-t3.commit();
 
 console.log('Balance:', myAccount.balance);
