60b8ec1d28b6eb45235978457680f00c78b72529
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-05 22:20:56 -0400

    add Transcation super class

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ class Account {
     this.userName = userName;
     this.balance = 0;
   }
-
-}
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
-class Deposite {
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account){
     this.amount = amount;
+    this.account = account;
   }
+}
+
+class Withdrawal extends Transaction {
 
   commit() {
-    balance += this.amount;
+   this.account.balance -= this.amount;
+  }
+
+}
+
+class Deposite extends Transaction{
+
+  commit() {
+    this.account.balance += this.amount;
   }
 }
 
@@ -34,16 +34,16 @@ class Deposite {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposite(120);
+t3 = new Deposite(120, myAccount);
 t3.commit();
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
